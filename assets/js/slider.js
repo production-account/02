25843cdd47aff8eaf11cd6abde3d30132fcb35ee
production-account <miwaSO@protonmail.com>
2026-07-22 16:50:33 +0900

    Add files via upload

---
+++ assets/js/slider.js
@@ -0,0 +1,124 @@
+"use strict";
+{
+
+  (function () {
+    const target = '.card .splide1';
+    const options = {
+      mediaQuery: 'min',
+      perMove: 1,
+      fixedWidth: '240px',
+      gap: 16,
+      updateOnMove: true,
+      breakpoints: {
+        1025: {
+          fixedWidth: '360px',
+          gap: 32,
+        }
+      },
+    }
+    const mySplide = new Splide(target, options);
+
+    mySplide.mount();
+  })();
+
+  (function () {
+    const target = '.card .splide2';
+    const options = {
+      mediaQuery: 'min',
+      perMove: 1,
+      fixedWidth: '240px',
+      gap: 16,
+      updateOnMove: true,
+      breakpoints: {
+        1025: {
+          fixedWidth: '360px',
+          gap: 32,
+        }
+      },
+    }
+    const mySplide = new Splide(target, options);
+
+    mySplide.mount();
+  })();
+
+  (function () {
+    const target = '.card .splide3';
+    const options = {
+      mediaQuery: 'min',
+      perMove: 1,
+      fixedWidth: '240px',
+      gap: 16,
+      updateOnMove: true,
+      breakpoints: {
+        1025: {
+          fixedWidth: '360px',
+          gap: 32,
+        }
+      },
+    }
+    const mySplide = new Splide(target, options);
+
+    mySplide.mount();
+  })();
+
+  (function () {
+    const target = '.card .splide4';
+    const options = {
+      mediaQuery: 'min',
+      perMove: 1,
+      fixedWidth: '240px',
+      gap: 16,
+      updateOnMove: true,
+      breakpoints: {
+        1025: {
+          fixedWidth: '360px',
+          gap: 32,
+        }
+      },
+    }
+    const mySplide = new Splide(target, options);
+
+    mySplide.mount();
+  })();
+
+  (function () {
+    const target = '.card .splide5';
+    const options = {
+      mediaQuery: 'min',
+      perMove: 1,
+      fixedWidth: '240px',
+      gap: 16,
+      updateOnMove: true,
+      breakpoints: {
+        1025: {
+          fixedWidth: '360px',
+          gap: 32,
+        }
+      },
+    }
+    const mySplide = new Splide(target, options);
+
+    mySplide.mount();
+  })();
+
+  (function () {
+    const target = '.card .splide6';
+    const options = {
+      mediaQuery: 'min',
+      perMove: 1,
+      fixedWidth: '240px',
+      gap: 16,
+      updateOnMove: true,
+      breakpoints: {
+        1025: {
+          fixedWidth: '360px',
+          gap: 32,
+        }
+      },
+    }
+    const mySplide = new Splide(target, options);
+
+    mySplide.mount();
+  })();
+}
+
